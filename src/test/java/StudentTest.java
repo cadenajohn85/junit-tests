edc7bd2deac84933fccf227b22e79bc12b3ea24b
John Cadena <cadena.john.85@gmail.com>
2019-10-08 14:16:47 -0500

    complete Step 3 of Part Two: Testing Legacy Code

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,13 +1,18 @@
+import org.junit.Before;
 import org.junit.Test;
-
-import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
 public class StudentTest {
+    Student fer;
+
+    @Before
+    public void setUp() {
+        fer = new Student(1L, "fer");
+    }
+
     @Test
     public void testCreateStudent(){
-        Student fer = new Student(1L, "fer");
         Student ryan = null;
         assertNull(ryan);
         assertNotNull(fer);
@@ -15,7 +20,6 @@ public class StudentTest {
 
     @Test
     public void testStudentFields(){
-        Student fer = new Student(1L, "fer");
         assertSame(1L, fer.getId());
         assertSame("fer", fer.getName());
         assertSame(0, fer.getGrades().size());
@@ -24,7 +28,6 @@ public class StudentTest {
 
     @Test
     public void testAddGrade(){
-        Student fer = new Student(1L, "fer");
         fer.addGrade(100);
         assertSame(100, fer.getGrades().get(0));
         fer.addGrade(80);
@@ -33,7 +36,6 @@ public class StudentTest {
 
     @Test
     public void testAverageGrade(){
-        Student fer = new Student(1L, "fer");
         fer.addGrade(100);
         fer.addGrade(80);
         assertEquals(90, fer.getGradeAverage(), 0);
